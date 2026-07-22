de2e22044f9cbf7693fd86e51ccfb93c076fba7f
samimkabiru <kabirusamimadeiza@gmail.com>
2026-07-22 15:49:03 +0100

    Initialize the taskflow project.

---
+++ src/main/java/com/theninjadev/taskflowapi/TaskflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.theninjadev.taskflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theninjadev/taskflowapi/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theninjadev.taskflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
